cbdf180467bc274a88bb0f311828e330f76140b8
Guilherme Souza <19gui94@gmail.com>
2019-04-18 22:25:45 +0200

    Export more data as part of the csv
    Use the 6 historical rankings

--- unzip_reformat_merge.py
+++ unzip_reformat_merge.py
@@ -15,10 +15,10 @@ df_2019 = pd.read_csv('raw_data/loto_201902.zip', sep=';')
 # old rang4 = 3 + 1 and 3 + 0 = new rang5 and rang6
 # old rang5 = 2 + 1 and 2 + 0 = new rang7 and rang8
 # old rang6 = 1 + 1 and 0 + 1 = new rang9
-# For simplicity, we'll only the three definitions that were kept stable overtime
+# We'll use the historical 6 categories and rename them
 
 
-def rename_and_cut(df, extra_columns):
+def handle_before_2017(df):
     columns = {
         'date_de_tirage': 'date',
         'boule_1': 'ball_1',
@@ -28,24 +28,44 @@ def rename_and_cut(df, extra_columns):
         'boule_5': 'ball_5',
         'numero_chance': 'lucky_ball',
         'nombre_de_gagnant_au_rang1': 'wins_5_1',
-        'nombre_de_gagnant_au_rang2': 'wins_5_0'
+        'nombre_de_gagnant_au_rang2': 'wins_5_0',
+        'nombre_de_gagnant_au_rang3': 'wins_4_1_and_4_0',
+        'nombre_de_gagnant_au_rang4': 'wins_3_1_and_3_0',
+        'nombre_de_gagnant_au_rang5': 'wins_2_1_and_2_0',
+        'nombre_de_gagnant_au_rang6': 'wins_1_1_and_0_1'
     }
-    columns.update(extra_columns)
     df = df.rename(columns=columns)
     return df[list(columns.values())]
 
 
-df_2008 = rename_and_cut(df_2008, {
-    'nombre_de_gagnant_au_rang6': 'wins_1_1_and_0_1'
-})
-df_2017 = rename_and_cut(df_2017, {
-    'nombre_de_gagnant_au_rang9': 'wins_1_1_and_0_1'
-})
-df_2019 = rename_and_cut(df_2019, {
-    'nombre_de_gagnant_au_rang9': 'wins_1_1_and_0_1'
-})
+def handle_after_2017(df):
+    columns = {
+        'date_de_tirage': 'date',
+        'boule_1': 'ball_1',
+        'boule_2': 'ball_2',
+        'boule_3': 'ball_3',
+        'boule_4': 'ball_4',
+        'boule_5': 'ball_5',
+        'numero_chance': 'lucky_ball',
+        'nombre_de_gagnant_au_rang1': 'wins_5_1',
+        'nombre_de_gagnant_au_rang2': 'wins_5_0',
+        'nombre_de_gagnant_au_rang9': 'wins_1_1_and_0_1'
+    }
+    new_df = df.rename(columns=columns)[list(columns.values())]
+    new_df['wins_4_1_and_4_0'] = df['nombre_de_gagnant_au_rang3'] + \
+        df['nombre_de_gagnant_au_rang4']
+    new_df['wins_3_1_and_3_0'] = df['nombre_de_gagnant_au_rang5'] + \
+        df['nombre_de_gagnant_au_rang6']
+    new_df['wins_2_1_and_2_0'] = df['nombre_de_gagnant_au_rang7'] + \
+        df['nombre_de_gagnant_au_rang8']
+    return new_df
 
-df = pd.concat([df_2008, df_2017, df_2019])
+
+df_2008 = handle_before_2017(df_2008)
+df_2017 = handle_after_2017(df_2017)
+df_2019 = handle_after_2017(df_2019)
+
+df = pd.concat([df_2008, df_2017, df_2019], sort=True)
 path = 'data/data.csv'
 df.to_csv(path, index=False)
 
